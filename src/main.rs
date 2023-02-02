b3e4eb0f4234fd411b698fabc0fb5b3e92e5faca
cmstoddard <christopher@stoddard.dev>
2023-02-01 21:56:49 -0600

    refactored code to check if player won, added crate to add logging

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use egui::Vec2;
 use std::collections::HashMap;
 
 fn main() {
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(530.0, 510.0)),
         ..Default::default()
@@ -17,6 +15,12 @@ fn main() {
     )
 }
 
+enum Direction {
+    Vertical,
+    Horizontal,
+    DiagonalLR,
+    DiagonalRL,
+}
 struct Board {
     board_layout: std::collections::HashMap<(i32, i32), BoardSlot>,
     turn: i32,
@@ -40,17 +44,16 @@ impl Default for Board {
                     y_coordinate: y_cord,
                     slot_value: String::from("  "),
                 };
-                //b.board_layout.push(bl);
                 b.board_layout.insert((x_cord, y_cord), bl);
             }
         }
         b
     }
 }
+
 impl Board {
     fn new() -> Self {
         Board {
-            //board_layout: Vec::new(),
             board_layout: HashMap::new(),
             turn: 0,
             win_state: false,
@@ -65,7 +68,6 @@ impl Board {
             ui.horizontal(|ui| {
                 for x_cord in 0..9 {
                     let current_slot = self.board_layout.get(&(x_cord, y_cord)).unwrap();
-                    //let b = egui::Button::new(format!("{}", x.slot_value))
                     let b = egui::Button::new(format!("{}", current_slot.slot_value))
                         .min_size(Vec2::new(50.0, 50.0));
                     if !self.win_state {
@@ -77,6 +79,7 @@ impl Board {
                             ))
                             .clicked()
                         {
+                            log::debug!("Selected: x: {}, y: {}", x_cord, y_cord);
                             self.change_value_slot(x_cord, y_cord);
                             self.check_if_won(x_cord, y_cord);
                         };
@@ -113,16 +116,20 @@ impl Board {
         }
     }
     fn check_if_won(&mut self, x: i32, y: i32) {
-        self.check_if_won_horizontally(x, y);
-        self.check_if_won_vertically(x, y);
-        self.check_if_won_diag_lr(x, y);
-        self.check_if_won_diag_rl(x, y);
+        //self.check_if_won_horizontally(x, y);
+        //self.check_if_won_vertically(x, y);
+        //self.check_if_won_diag_lr(x, y);
+        //self.check_if_won_diag_rl(x, y);
+        self.meme(x, y, Direction::Vertical);
+        self.meme(x, y, Direction::Horizontal);
+        self.meme(x, y, Direction::DiagonalLR);
+        self.meme(x, y, Direction::DiagonalRL);
     }
 
     //TODO: you could make an enum with the values of x and y, and the vector that you want checked
     //to get rid of the 4 functions
-    fn check_if_won_vertically(&mut self, x: i32, y: i32) {
-        //check if won vertically
+
+    fn meme(&mut self, x: i32, y: i32, direction: Direction) {
         if let Some(value) = self.board_layout.get(&(x, y)) {
             //looking for 4 in a sequence
             let mut iterations = 0;
@@ -132,159 +139,129 @@ impl Board {
             let mut direction_of_needle = 0;
             //consecutive slots of the same value
             let mut count = 1;
-
             let mut winning_slots: Vec<(i32, i32)> = Vec::new();
-            while iterations < 4 {
-                if direction_of_needle == 0 {
-                    if let Some(vert_up) = self.board_layout.get(&(x, y - needle)) {
-                        if value.slot_value == vert_up.slot_value {
-                            winning_slots.push((x, y - needle));
-                            count += 1;
-                            needle += 1;
+
+            match direction {
+                Direction::Vertical => {
+                    while iterations < 4 {
+                        if direction_of_needle == 0 {
+                            if let Some(vert_up) = self.board_layout.get(&(x, y - needle)) {
+                                if value.slot_value == vert_up.slot_value {
+                                    winning_slots.push((x, y - needle));
+                                    count += 1;
+                                    needle += 1;
+                                } else {
+                                    direction_of_needle = 1;
+                                    needle = 1;
+                                }
+                            }
                         } else {
-                            direction_of_needle = 1;
-                            needle = 1;
+                            if let Some(vert_up) = self.board_layout.get(&(x, y + needle)) {
+                                if value.slot_value == vert_up.slot_value {
+                                    winning_slots.push((x, y + needle));
+                                    count += 1;
+                                    needle += 1;
+                                } else {
+                                    break;
+                                }
+                            }
                         }
-                    }
-                } else {
-                    if let Some(vert_up) = self.board_layout.get(&(x, y + needle)) {
-                        if value.slot_value == vert_up.slot_value {
-                            winning_slots.push((x, y + needle));
-                            count += 1;
-                            needle += 1;
-                        } else {
-                            break;
-                        }
+                        iterations += 1;
                     }
                 }
-                iterations += 1;
+                Direction::Horizontal => {
+                    while iterations < 4 {
+                        if direction_of_needle == 0 {
+                            if let Some(vert_up) = self.board_layout.get(&(x - needle, y)) {
+                                if value.slot_value == vert_up.slot_value {
+                                    winning_slots.push((x - needle, y));
+                                    count += 1;
+                                    needle += 1;
+                                } else {
+                                    direction_of_needle = 1;
+                                    needle = 1;
+                                }
+                            }
+                        } else {
+                            if let Some(vert_up) = self.board_layout.get(&(x + needle, y)) {
+                                if value.slot_value == vert_up.slot_value {
+                                    winning_slots.push((x + needle, y));
+                                    count += 1;
+                                    needle += 1;
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                        iterations += 1;
+                    }
+                }
+                Direction::DiagonalLR=> {
+                    while iterations < 4 {
+                        if direction_of_needle == 0 {
+                            if let Some(vert_up) = self.board_layout.get(&(x + needle, y - needle))
+                            {
+                                if value.slot_value == vert_up.slot_value {
+                                    winning_slots.push((x + needle, y - needle));
+                                    count += 1;
+                                    needle += 1;
+                                } else {
+                                    direction_of_needle = 1;
+                                    needle = 1;
+                                }
+                            }
+                        } else {
+                            if let Some(vert_up) = self.board_layout.get(&(x - needle, y + needle))
+                            {
+                                if value.slot_value == vert_up.slot_value {
+                                    winning_slots.push((x - needle, y + needle));
+                                    count += 1;
+                                    needle += 1;
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                        iterations += 1;
+                    }
+                }
+                Direction::DiagonalRL=> {
+                    while iterations < 4 {
+                        if direction_of_needle == 0 {
+                            if let Some(vert_up) = self.board_layout.get(&(x + needle, y + needle))
+                            {
+                                if value.slot_value == vert_up.slot_value {
+                                    winning_slots.push((x + needle, y + needle));
+                                    count += 1;
+                                    needle += 1;
+                                } else {
+                                    direction_of_needle = 1;
+                                    needle = 1;
+                                }
+                            }
+                        } else {
+                            if let Some(vert_up) = self.board_layout.get(&(x - needle, y - needle))
+                            {
+                                if value.slot_value == vert_up.slot_value {
+                                    winning_slots.push((x - needle, y - needle));
+                                    count += 1;
+                                    needle += 1;
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                        iterations += 1;
+                    }
+                }
             }
+
             if count == 4 {
                 self.win_state = true;
                 winning_slots.push((x, y));
                 self.win_slots = winning_slots;
             } else {
                 winning_slots.clear();
-            }
-        }
-    }
-
-    fn check_if_won_horizontally(&mut self, x: i32, y: i32) {
-        //check if won horizontally
-        if let Some(value) = self.board_layout.get(&(x, y)) {
-            //looking for 4 in a sequence
-            let mut iterations = 0;
-            //this is the distance from the origin, to the current point
-            let mut needle = 1;
-            //0 is moving left, 1 is moving right to search
-            let mut direction_of_needle = 0;
-            //consecutive slots of the same value
-            let mut count = 1;
-            while iterations < 4 {
-                if direction_of_needle == 0 {
-                    if let Some(vert_up) = self.board_layout.get(&(x - needle, y)) {
-                        if value.slot_value == vert_up.slot_value {
-                            count += 1;
-                            needle += 1;
-                        } else {
-                            direction_of_needle = 1;
-                            needle = 1;
-                        }
-                    }
-                } else {
-                    if let Some(vert_up) = self.board_layout.get(&(x + needle, y)) {
-                        if value.slot_value == vert_up.slot_value {
-                            count += 1;
-                            needle += 1;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                iterations += 1;
-            }
-            if count == 4 {
-                self.win_state = true;
-            }
-        }
-    }
-
-    fn check_if_won_diag_lr(&mut self, x: i32, y: i32) {
-        if let Some(value) = self.board_layout.get(&(x, y)) {
-            //looking for 4 in a sequence
-            let mut iterations = 0;
-            //this is the distance from the origin, to the current point
-            let mut needle = 1;
-            //0 is moving left, 1 is moving right to search
-            let mut direction_of_needle = 0;
-            //consecutive slots of the same value
-            let mut count = 1;
-
-            while iterations < 4 {
-                if direction_of_needle == 0 {
-                    if let Some(vert_up) = self.board_layout.get(&(x - needle, y + needle)) {
-                        if value.slot_value == vert_up.slot_value {
-                            count += 1;
-                            needle += 1;
-                        } else {
-                            direction_of_needle = 1;
-                            needle = 1;
-                        }
-                    }
-                } else {
-                    if let Some(vert_up) = self.board_layout.get(&(x + needle, y - needle)) {
-                        if value.slot_value == vert_up.slot_value {
-                            count += 1;
-                            needle += 1;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                iterations += 1;
-            }
-            if count == 4 {
-                self.win_state = true;
-            }
-        }
-    }
-
-    fn check_if_won_diag_rl(&mut self, x: i32, y: i32) {
-        if let Some(value) = self.board_layout.get(&(x, y)) {
-            //looking for 4 in a sequence
-            let mut iterations = 0;
-            //this is the distance from the origin, to the current point
-            let mut needle = 1;
-            //0 is moving left, 1 is moving right to search
-            let mut direction_of_needle = 0;
-            //consecutive slots of the same value
-            let mut count = 1;
-
-            while iterations < 4 {
-                if direction_of_needle == 0 {
-                    if let Some(vert_up) = self.board_layout.get(&(x + needle, y + needle)) {
-                        if value.slot_value == vert_up.slot_value {
-                            count += 1;
-                            needle += 1;
-                        } else {
-                            direction_of_needle = 1;
-                            needle = 1;
-                        }
-                    }
-                } else {
-                    if let Some(vert_up) = self.board_layout.get(&(x - needle, y - needle)) {
-                        if value.slot_value == vert_up.slot_value {
-                            count += 1;
-                            needle += 1;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                iterations += 1;
-            }
-            if count == 4 {
-                self.win_state = true;
             }
         }
     }
